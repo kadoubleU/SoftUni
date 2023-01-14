6698a0ce421d345cc16309fe06a94ddb0d8a421a
kadoubleU <108259380+kadoubleU@users.noreply.github.com>
2023-01-14 12:50:04 +0200

    Add files via upload

---
+++ 01. JavaScript Basics/09-09. numFrom1To10.js
@@ -0,0 +1,11 @@
+function numFrom1To10(input) {
+    let num = 1;
+    while(num <= 10) {
+        console.log(num);
+        num++;
+    }
+}
+
+numFrom1To10();
+
+//Напишете програма, която отпечатва числата от 1 до 10, по едно на ред.
