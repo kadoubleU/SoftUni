307c6738328a0536a1e7217318c1fdbb0579515c
kadoubleU <delievabg@gmail.com>
2023-05-31 12:15:37 +0300

    New Excercise solved

---
+++ 02.Programming-Fundamentals-with-JavaScript/05. Arrays/01.sumFirstAndLastArrayElements.js
@@ -0,0 +1,10 @@
+function arr(numbers) {
+    let first = numbers[0];
+    let last = numbers[numbers.length - 1];
+
+    console.log(first+last);
+}
+
+arr([20, 30, 40]);
+arr([10, 17, 22, 33]);
+arr([11, 58, 69]);
