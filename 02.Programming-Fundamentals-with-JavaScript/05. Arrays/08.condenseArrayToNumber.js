c315c26073e853b4a2c7e88d3cd654720e2b0622
kadoubleU <delievabg@gmail.com>
2023-06-01 20:19:52 +0300

    New solution added

--- 02.Programming-Fundamentals-with-JavaScript/05. Arrays/08.condenseArrayToNumber.js
+++ 02.Programming-Fundamentals-with-JavaScript/05. Arrays/08.condenseArrayToNumber.js
@@ -1,7 +1,14 @@
-function condenseArrayToNumber() {
-
+function condenseArrayToNumber(arr) {
+    while (arr.length > 1) {
+        let condensed = Array(arr.length-1);
+        for (let i = 0; i < arr.length - 1; i++) {
+            condensed[i] = Number(arr[i]) + Number(arr[i + 1]);
+        }
+        arr = condensed;
+    }
+    console.log(arr[0])
 }
 
-condenseArrayToNumber([2,10,3]);
-condenseArrayToNumber([5,0,4,1,2] );
+condenseArrayToNumber([2, 10, 3]);
+condenseArrayToNumber([5, 0, 4, 1, 2]);
 condenseArrayToNumber([1]);
