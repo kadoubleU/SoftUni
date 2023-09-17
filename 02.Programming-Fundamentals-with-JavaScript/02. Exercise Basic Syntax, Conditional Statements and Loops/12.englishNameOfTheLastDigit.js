072beb34e04cadead8393fe4d87b902710b3ad22
Katya Delieva <108259380+kadoubleU@users.noreply.github.com>
2023-09-17 15:56:26 +0300

    Update 12.englishNameOfTheLastDigit.js

--- 02.Programming-Fundamentals-with-JavaScript/02. Exercise Basic Syntax, Conditional Statements and Loops/12.englishNameOfTheLastDigit.js
+++ 02.Programming-Fundamentals-with-JavaScript/02. Exercise Basic Syntax, Conditional Statements and Loops/12.englishNameOfTheLastDigit.js
@@ -35,6 +35,39 @@ function enNameLastDigit(num) {
     console.log(name);
 }
 
+//SECOND VARIANT
+
+function englishName(num) {
+
+    let numberToString = num.toString();
+
+    let array = numberToString.split("");
+   
+    let lastDigit = Number(array[array.length - 1]);
+
+    if(lastDigit === 1) {
+        console.log("one");
+    }else if(lastDigit === 2) {
+        console.log("two");
+    }else if(lastDigit === 3) {
+        console.log("three")
+    }else if(lastDigit === 4) {
+        console.log("four")
+    }else if(lastDigit === 5) {
+        console.log("five")
+    }else if(lastDigit === 6) {
+        console.log("six");
+    }else if(lastDigit === 7) {
+        console.log("seven");
+    }else if(lastDigit === 8) {
+        console.log("eight");
+    }else if(lastDigit === 9) {
+        console.log("nine");
+    }else if(lastDigit === 0) {
+        console.log("zero");
+    }
+}
+
 enNameLastDigit(512);
 console.log("------");
 enNameLastDigit(1);
